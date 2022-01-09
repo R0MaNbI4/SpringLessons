c39095e09093353b0c923f100bcd0ed1ba574535
R0MaNbI4 <wb15935725@mail.ru>
2022-01-09 22:27:10 +0800

    lesson10

---
+++ lesson10/HW/src/main/java/ru/geekbrains/thymeleafdemo/model/Product.java
@@ -0,0 +1,135 @@
+package ru.geekbrains.thymeleafdemo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotEmpty
+    @Column(name = "article")
+    private String article;
+
+    @NotEmpty
+    @Column(name = "title")
+    private String title;
+
+    @NotEmpty
+    @Column(name = "price")
+    private String price;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "color")
+    private String color;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    public Product() {
+    }
+
+    public Product(String article, String title, String price, String category, String brand, String model, String color, String imageUrl) {
+        this.article = article;
+        this.title = title;
+        this.price = price;
+        this.category = category;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getArticle() {
+        return article;
+    }
+
+    public void setArticle(String article) {
+        this.article = article;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
